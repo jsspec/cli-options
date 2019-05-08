f06c25675d405bff1e3838dc1bddd8317433e3b2
HookyQR <hooky@qrmaster.co.nz>
2019-05-08 16:02:16 +0800

    Initial commit

---
+++ test/options.test.js
@@ -0,0 +1,41 @@
+const Option = require('../lib/options');
+
+describe('Option', () => {
+  let options = {
+    random: { alias: 'R', type: Boolean, required: false, default: true },
+    require: { alias: 'r', type: Array, required: false, default: [] },
+    format: { alias: 'f', type: String, required: false, default: 'documentation', },
+    files: { type: Array, required: false, default: [] }
+  };
+
+  it('picks up a simple arg set correctly', () => {
+    let args = 'node ./index.js -r ./some/file ./some/otherFile -fd run/this/test and/this/one'.split(' ');
+    let option = new Option(args, options, 'files');
+    console.log(option.settings);
+    expect(option.settings).to.deep.include({
+      random: true,
+      require: ['./some/file', './some/otherFile'],
+      format: 'd',
+      files: ['run/this/test', 'and/this/one']
+    });
+  });
+
+  it('picks up a both parts of an arrray correctly', () => {
+    let args = 'node ./index.js --require=./some/file ./some/otherFile -fd run/this/test and/this/one'.split(' ');
+    let option = new Option(args, options, 'files');
+    expect(option.settings).to.deep.include({
+      random: true,
+      require: ['./some/file', './some/otherFile'],
+      format: 'd',
+      files: ['run/this/test', 'and/this/one']
+    });
+  });
+
+  it('fails on an unknown and toggles flags', () => {
+    let args = 'node ./index.js --youDontKnowMe -RR'.split(' ');
+    let option = new Option(args, options, 'files');
+    expect(option.settings).to.deep.include({ random: false });
+    expect(option.errors).to.have.length(1);
+    expect(option.errors[0]).to.match(/unknown argument 'youDontKnowMe'/);
+  });
+});
---
+++ test/parser.test.js
@@ -0,0 +1,89 @@
+const Parser = require('../lib/parser.js/index.js');
+
+describe('Parser', () => {
+  describe('construction', () => {
+    context('valid', () => {
+      it('has no errors', () => {
+        let options = {
+          _: { multiple: true }
+        };
+        let parser = new Parser(options);
+        expect(parser.errors).to.have.length(0);
+        expect(parser.settings).to.have.all.keys('_');
+      });
+    });
+
+    context('missing catch all', () => {
+      it('advises via error', () => {
+        let options = {};
+        let parser = new Parser(options);
+        expect(parser.errors).to.have.length(1);
+        expect(parser.errors[0]).to.match(/Catch all ".*" not in options group/);
+      });
+    });
+    context('short options', () => {
+      it('sets shortOptions to long version', () => {
+        let options = {
+          _: { type: Array },
+          test: { alias: 't' }
+        };
+        let parser = new Parser(options);
+        expect(parser.errors).to.have.length(0);
+        expect(parser.alias.t).to.eql('test');
+      });
+    });
+  });
+  describe('#parse', () => {
+    let parser;
+    beforeEach(() => {
+      parser = new Parser({
+        _: { type: Array },
+        hasShort: { alias: 'h', type: Array, default: ['SHORT'] },
+        isFlag: { type: Boolean, alias: 'f' },
+        longOnly: { type: String, default: 'LONG' }
+      });
+    });
+    describe('correct use', () => {
+      it('uses catch all after -', () => {
+        const result = parser.parse(['-h', 'test', '-', 'test2']);
+        expect(parser.errors).to.have.length(0);
+        expect(result).to.deep.include({ hasShort: ['test'], _: ['test2'] });
+      });
+      it('uses catch all after --', () => {
+        const result = parser.parse(['-h', 'test', '--', 'test2']);
+        expect(parser.errors).to.have.length(0);
+        expect(result).to.deep.include({ hasShort: ['test'], _: ['test2'] });
+      });
+      it('ignores `options` after --', () => {
+        const result = parser.parse(['-h', 'test', '--', 'test2', '--longOnly', 'test3']);
+        expect(parser.errors).to.have.length(0);
+        expect(result).to.deep.include({ hasShort: ['test'], _: ['test2', '--longOnly', 'test3'] });
+      });
+      it('sets defaults', () => {
+        const result = parser.parse(['-f', 'test3']);
+        expect(parser.errors).to.have.length(0);
+        expect(result).to.deep.include({ hasShort: ['SHORT'], isFlag: true, longOnly: 'LONG', _: ['test3'] });
+      });
+      it('catches following values', () => {
+        const result = parser.parse(['-fh', 'test', 'test2', '--longOnly', 'test3', 'test4']);
+        expect(parser.errors).to.have.length(0);
+        expect(result).to.deep.include({ hasShort: ['test', 'test2'], isFlag: true, longOnly: 'test3', _: ['test4'] });
+      });
+      it('catches attached values', () => {
+        const result = parser.parse(['-fh=test', 'test2', '--longOnly=test3', 'test4']);
+        expect(parser.errors).to.have.length(0);
+        expect(result).to.deep.include({ hasShort: ['test', 'test2'], isFlag: true, longOnly: 'test3', _: ['test4'] });
+      });
+    });
+
+    context('incorrect use', () => {
+      it('reports unknown options', () => {
+        parser.parse(['-X', '--what']);
+        expect(parser.errors).to.eql([
+          'unknown shorthand argument \'X\'',
+          'unknown argument \'what\''
+        ]);
+      });
+    });
+  });
+});
